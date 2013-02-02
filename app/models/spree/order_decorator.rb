69fcdb478b2f4957b03e881116ef8cc20b306371
Kirill Zonov <graffzon@gmail.com>
2013-02-02 23:02:13 +0400

    modified order decorator

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,15 +2,27 @@ Spree::Order.class_eval do
 
   attr_accessible :state, :completed_at
 
-  def run_states
-    ActiveRecord::Base.transaction do
-      update!
-      finalize!
-      create_shipment!
-      adjustments.each do |adj|
-        adj.update!
-      end
-     update_attributes(:state => 'complete', :completed_at => Time.now)
-   end
+  checkout_flow do
+    go_to_state :finish
   end
+
+  def available_shipping_methods(display_on = nil)
+    Spree::ShippingMethod.all
+  end
+
+  def finalize!
+    update_attribute(:completed_at, Time.now)
+    Spree::InventoryUnit.assign_opening_inventory(self)
+    # lock any optional adjustments (coupon promotions, etc.)
+    adjustments.optional.each { |adjustment| adjustment.update_attribute('locked', true) }
+    deliver_order_confirmation_email
+
+    self.state_changes.create({
+      :previous_state => 'cart',
+      :next_state     => 'complete',
+      :name           => 'order' ,
+      :user_id        => (Spree::User.respond_to?(:current) && Spree::User.current.try(:id)) || self.user_id
+    }, :without_protection => true)
+  end
+
 end
