f824ce704093eaeebb2296011fc0e6d30ab7fb83
Volodya Protasevich <ggg1321@gmail.com>
2013-02-08 17:37:08 +0400

    Update override and convert checkout form to erb

--- app/overrides/checkout_form.rb
+++ app/overrides/checkout_form.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/orders/edit",
                      :insert_after => "[data-hook = 'outside_cart_form']",
                      :partial => "spree/orders/checkout_form",
-                     :name => "checkout_form")
+                     :name => "checkout_form",
+                     :original => '0b5774f437a257c218490c5846dafd63ea5909a1')
