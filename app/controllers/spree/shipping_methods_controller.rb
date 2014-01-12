64a95fb5f7c43813a8012853e96a8bd772cb80df
Mahys116 <mahys116@gmail.com>
2014-01-12 18:18:31 +0400

    update shipping rate selec

--- app/controllers/spree/shipping_methods_controller.rb
+++ app/controllers/spree/shipping_methods_controller.rb
@@ -18,8 +18,11 @@ module Spree
         current_order.ship_address.country_id = params['country_id']
         Float(params['state']) != nil rescue false ? current_order.ship_address.state_id = params['state'] : current_order.ship_address.state_name = params['state']
         current_order.save(:validate => false)
-        current_order.rate_hash.each { |rh| rh.cost = Money.new(rh.cost, { :currency => rh.currency}).to_s }
-        render :json => current_order.rate_hash
+        
+        shipping_rates = []
+        Spree::ShippingRate.find(current_order.tax_zone.shipping_methods.map(&:id)).each { |rate| shipping_rates.push( r = {name: rate.name, cost: rate.display_cost}) }
+        
+        render :json => shipping_rates
       else
         render :json => nil
       end
